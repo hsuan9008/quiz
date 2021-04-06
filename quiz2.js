879c9786aa7bb27529f2ee67c70ea34647f510c9
hsuan9008 <b0829032@cgu.edu.tw>
2021-04-06 20:58:59 +0800

    Update quiz2.js

--- quiz2.js
+++ quiz2.js
@@ -1,4 +1,16 @@
-document.addEventListener("keydown",function(event){
-    alert(event.key);
-    // document.write(event.key);
-})
+var tags = "bird";
+
+var dataUrl = "https://api.flickr.com/services/feeds/photos\_public.gne?tags=" + tags
++ "&tagmode=any&format=json&per_page=400&jsoncallback=?"; //public的斜線
+var data = $.getJSON(dataUrl);
+data.done(function (msg) {
+    $.each(msg.items, function (i, item) {
+        $("#contain").html();
+        $("#contain").append($("<img/>").attr("src", item.media.m));
+    });
+});
+
+data.fail(function (msg) {
+    console.log(msg);
+    $("#contain").html("fail getting data");
+});
